de624a32a4af8487c3a69b6201a9b6061628a9d0
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-12-05 23:41:18 +0000

    com eu fiz

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "deu certo" });
+  const updateAt = new Date().toISOString();
+  const resut = await database.query("SELECT version();");
+  const versionAt = resut.rows[0].version;
+  const resutMax = await database.query("Show Max_connections");
+  const maxConnections = resutMax.rows[0];
+  const resutActivity = await database.query(
+    "SELECT count(*) FROM pg_stat_activity; ",
+  );
+  const activeConnections = resutActivity.rows[0];
+
+  response.status(200).json({
+    update_at: updateAt,
+    version_at: versionAt,
+    max_connections: maxConnections,
+    active_connections: activeConnections,
+  });
 }
 
 export default status;
--- teste/integration/api/v1/status/get.test.js
+++ teste/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("Get to /api/v1/status/ should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+  expect(responseBody.version_at).toEqual(
+    "PostgreSQL 16.9 on x86_64-pc-linux-musl, compiled by gcc (Alpine 13.2.1_git20240309) 13.2.1 20240309, 64-bit",
+  );
 });
