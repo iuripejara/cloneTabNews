bcea0caa6c6dc09f1550d23e5831bd26f1b66fb1
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-12-05 00:02:15 +0000

    mover file to

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
   const result = await client.query(queryObject);
   await client.end();
