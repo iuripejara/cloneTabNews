167015bb320f75fe4417560871ef92dcb4e41514
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-12-08 21:01:01 +0000

    finsh /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,20 +2,29 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-  const resut = await database.query("SELECT version();");
-  const versionAt = resut.rows[0].version;
+  const resut = await database.query("SHOW server_version;");
+  const versionAt = resut.rows[0].server_version;
+
   const resutMax = await database.query("Show Max_connections");
-  const maxConnections = resutMax.rows[0];
-  const resutActivity = await database.query(
-    "SELECT count(*) FROM pg_stat_activity; ",
-  );
-  const activeConnections = resutActivity.rows[0];
+  const maxConnections = resutMax.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const resutActivity = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnections = resutActivity.rows[0].count;
 
   response.status(200).json({
     update_at: updateAt,
-    version_at: versionAt,
-    max_connections: maxConnections,
-    active_connections: activeConnections,
+    dependencies: {
+      database: {
+        version: versionAt,
+        max_connections: parseInt(maxConnections),
+        active_connections: activeConnections,
+      },
+    },
   });
 }
 
--- teste/integration/api/v1/status/get.test.js
+++ teste/integration/api/v1/status/get.test.js
@@ -3,11 +3,10 @@ test("Get to /api/v1/status/ should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
-  expect(responseBody.version_at).toEqual(
-    "PostgreSQL 16.9 on x86_64-pc-linux-musl, compiled by gcc (Alpine 13.2.1_git20240309) 13.2.1 20240309, 64-bit",
-  );
+  expect(responseBody.dependencies.database.version).toEqual("16.9");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.active_connections).toEqual(1);
 });
